32f932e0094b1ff47954eec0990f1b586e6d5ac5
Ушаков Егор Леонидович <pl8750@C201N9.pl1.edu>
2022-04-02 12:49:24 +0500

    save

--- pages/category.js
+++ pages/category.js
@@ -1,16 +1,64 @@
-import Image from 'next/image'
-import Menu from '../components/menu'
-import home1 from '../images/home1-img.png'
+import Image from "next/image"
+import Menu from "./menu"
+import cat1 from "../images/category1-img.png"
+import cat2 from "../images/category2-img.png"
+import cat3 from "../images/category3-img.png"
 
 export default function Category() {
   return (
     <div className="site">
       <div className="site__wrapper">
         <div className="site__menu">
-          <Menu />
+            <Menu />
         </div>
         <div className="site__content">
-          Категории
+            <div className="site__category">
+                <div className="category__title">
+                    <p>Favorite Scare</p>
+                    <p>Category</p>
+                </div>
+                <div className="category__block">
+                    <div className="category__product">
+                        <Image 
+                            src={cat1}
+                            width="250px"
+                            height="250px"
+                        />
+                        <p className="product__title">
+                            Ghosts
+                        </p>
+                        <p className="product__subtitle">
+                            Choose the ghosts, <br/> the scariest there are.
+                        </p>
+                    </div>
+                    <div className="category__product">
+                        <Image 
+                            src={cat2}
+                            width="250px"
+                            height="250px"
+                        />
+                        <p className="product__title">
+                            Pumpkings
+                        </p>
+                        <p className="product__subtitle">
+                            You look ot the scariest <br/> pumpkins there is.
+                        </p>
+                    </div>
+                    <div className="category__product">
+                        <Image 
+                            src={cat3}
+                            width="250px"
+                            height="250px"
+                        />
+                        <p className="product__title">
+                            Witch Hat
+                        </p>
+                        <p className="product__subtitle">
+                            Pick the most stylish witch <br/> hots out there
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import Menu from '../components/menu'
-import home1 from '../images/home1-img.png'
+import Image from "next/image"
+import home1 from "../images/home1-img.png"
+import Menu from "./menu"
 
 export default function Index() {
   return (
@@ -10,37 +10,39 @@ export default function Index() {
           <Menu />
         </div>
         <div className="site__content">
-          <div className="site__slider">
-            <div className="slider__list">
-              <div className="slider__block">
-                <div className="block__image">
-                  <Image
-                    src={home1}
-                    width="460px"
-                    height="500px"
-                  />
+            <div className="site__slider">
+                <div className="slider__list">
+                  <div className="slider__block">
+                    <div className="block__image">
+                      <Image
+                        src={home1}
+                        width="460px"
+                        height="500px"
+                      />
+                    </div>
+                    <div className="block__info">
+                      <h1 class="info__title">
+                      UOOOO
+                      <br/>
+                      TRICK OR
+                      <br/>
+                      TREAT!!
+                      </h1>
+                      <p class="info__description">
+                        Hello world! I'am Hellowen Steve. 
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="block__info">
-                  <h1 class="info__title">
-                    UOOOO
-                    <br />
-                    TRICK OR
-                    <br />
-                    TREAT!!
-                  </h1>
-                  <p class="info__description">
-                    Hi, I'm Reiza, people call me "El Labu". I am currently trying
-                    to learn something new, building my own bike with parts made
-                    only in Malaysia.
-                  </p>
+                <div className="slider__menu">
+                  <div className="menu__mark">
+
+                  </div>
+                  <div className="menu__mark">
+
+                  </div>
                 </div>
-              </div>
             </div>
-            <div className="slider__menu">
-              <div className="menu__mark"></div>
-              <div className="menu__mark"></div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
